c6bc988bd593598d02e091f6a49695b3cf622375
karishma kumar <64687364+karishmakumar@users.noreply.github.com>
2025-07-17 10:47:50 +0100

    Initial commit

---
+++ src/com/karishma/sum.java
@@ -0,0 +1,13 @@
+package com.karishma;
+
+import java.util.Scanner;
+
+public class sum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+
+        System.out.println(a+b);
+    }
+}
